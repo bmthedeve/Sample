76d1203e16b05524dad3a3a77c39358242216577
Balamurugan <95612809+bmthedeve@users.noreply.github.com>
2022-01-19 10:44:34 +0530

    Update First.java

--- src/First.java
+++ src/First.java
@@ -1,6 +1,6 @@
 
 public class First {
 public static void main(String[] args) {
-	System.out.println("First line edited in GH for second time");
+	System.out.println("First line edited in GH fo");
 }
 }
